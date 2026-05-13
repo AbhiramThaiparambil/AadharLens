3eca5d7ad3aa46d26a887cbea829f489d8034e6c
Abhiram <abhiram@Abhirams-MacBook-Air.local>
2026-05-14 05:14:29 +0530

    feat(backend): create routes folder and implement main routes

---
+++ server/src/presentation/routes/aadhaar.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { container } from '../../container/container.js';
+import { AadhaarController } from '../controllers/aadhaar.controller.js';
+import { upload } from '../middlewares/upload.middleware.js';
+const router = Router();
+const aadhaarController = container.resolve(AadhaarController);
+router.get('/', aadhaarController.getWelcomeMessage);
+router.post(
+  '/parse-aadhaar',
+  upload.fields([
+    { name: 'frontFile', maxCount: 1 },
+    { name: 'backFile',  maxCount: 1 },
+  ]),
+  aadhaarController.parseAadhaar,
+);
+export default router;
