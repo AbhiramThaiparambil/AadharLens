0f3a39276d4c1ae9d6cccb3a1e8f8ded554922d8
AbhiramTB <abhiramthaipparambil@gmail.com>
2026-05-14 22:44:14 +0530

    feat(backend): register services in Tsyringe DI container

---
+++ server/src/container/container.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { SharpImageProcessor }  from '../services/image-processor/image-processor.service.js';
+import { TesseractOcrService }  from '../services/ocr/ocr.service.js';
+import { AadharFieldExtractor } from '../services/extractor/extractor.service.js';
+import { ZodAadhaarValidator }  from '../services/validator/validator.service.js';
+import { ParseAadhaarUseCase }  from '../application/use-cases/parse-aadhaar/parse-aadhaar.usecase.js';
+container.register('IImageProcessor',        { useClass: SharpImageProcessor });
+container.register('IOcrService',            { useClass: TesseractOcrService });
+container.register('IExtractorService',      { useClass: AadharFieldExtractor });
+container.register('IValidatorService',      { useClass: ZodAadhaarValidator });
+container.register('IParseAadhaarUseCase',   { useClass: ParseAadhaarUseCase });
+export { container };
