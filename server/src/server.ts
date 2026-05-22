a1c9baeb6b144ba5d06dd46529eb00c34f80d430
AbhiramTB <abhiramthaipparambil@gmail.com>
2026-05-23 02:35:54 +0530

    integrate frontend build with typescript backend server production setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import { env } from './config/env.js';
 import aadhaarRoutes from './presentation/routes/aadhaar.routes.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+console.log(import.meta)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+let clientPath=path.join(path.resolve(__dirname),"../../client/dist")
+
+
+
+
+app.use(express.static(clientPath));
+
 app.use('/api', aadhaarRoutes);
+
+app.get("/{*wildcard}", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
+
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT} in ${env.NODE_ENV} mode`);
 });
