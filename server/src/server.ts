dace42f7934d0ba317a343b6af1b48af9a8a4d87
Abhiram <abhiram@Abhirams-MacBook-Air.local>
2026-06-30 22:37:05 +0530

    refactor: add global error handling middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { env } from './config/env.js';
 import aadhaarRoutes from './presentation/routes/aadhaar.routes.js';
+import { errorHandler } from './presentation/middlewares/errorHandler.js';
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use('/api', aadhaarRoutes);
+app.use(errorHandler)
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT} in ${env.NODE_ENV} mode`);
 });
